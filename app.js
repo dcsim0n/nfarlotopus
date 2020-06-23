78f90a441cc26ff07174d5412e53b99d0dad434f
Dana Simmons <dsanalog@gmail.com>
2020-06-23 14:04:21 -0400

    Parse URLs from previous messages
    Why: experimenting with various ways to parse messages
    
    *---
    
    How: defined a regex to use for identifying urls, then iterate over recent messages
    
    *---

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const URLREGEX = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+
+const URLS = [];
+
 client.on('ready', ( )=>{
   console.log(`Logged in as: ${client.user.tag}`);
   
@@ -17,11 +21,24 @@ client.on('ready', ( )=>{
     //   console.log(`Found message: ${m.content}`)
     // })
 
-    // Collect last 10 messages
+    // Fetch recent messages and extract links and URLS
     msgMgr = new Discord.MessageManager(chan)
-    msgMgr.fetch({limit: 10})
-    .then( m =>{
-      console.log(m.array().length);
+    msgMgr.fetch({limit: 100})
+    .then( allMessages =>{
+      // allMessages is a Collection Map 
+      for (const msg of allMessages) {
+
+        console.log("Checking msg id:", msg[0])
+
+        urls = msg[1].content.match(URLREGEX);
+
+        if(urls){
+          URLS.push(...urls);
+        }
+      }
+      console.log(URLS);
+      // When complete, log out
+      client.destroy();
     })
   })
 });
